daceab76a6f933658250979e6374917e6ff9ee54
Long Lane <tech@long-lane.co.uk>
2023-10-05 22:45:56 +0100

    comment out base from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/broughty_ferry_community/'
-
-// })
+export default defineConfig({
+  plugins: [react()],
+});
