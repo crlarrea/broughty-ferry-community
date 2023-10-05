1aeb3e197ee6c78dce9cfa8ff77900384a8bcabe
Long Lane <tech@long-lane.co.uk>
2023-10-05 22:41:36 +0100

    comment out base from vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/broughty_ferry_community/'
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/broughty_ferry_community/'
 
-})
+// })
